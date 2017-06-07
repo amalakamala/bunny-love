a40a7442c52c7ab770b90c8bf3da3b01026e0547
amalakamala <amalakamala.a@gmail.com>
2017-06-06 21:06:17 -0400

    responsive

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById("menu-hamburguesa");
+
+menu.addEventListener("click",function(){
+	var elnav = document.getElementById("nav-menu-js");
+	elnav.classList.toggle("el-hide");
+
+
+})
